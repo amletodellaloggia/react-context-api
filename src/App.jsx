ca350ab2fa4fa34eb9d511d88f26908e9dc9d1c2
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-09 13:47:20 +0200

    [upd] imported BudgetProvider, wrapped Routes in BudgetProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import { BrowserRouter } from "react-router-dom";
-import { Routes } from "react-router-dom";
-import { Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Homepage from "./pages/Homepage";
 import ChiSiamo from "./pages/ChiSiamo";
 import Prodotti from "./pages/Prodotti";
 import DefaultLayout from "./layouts/DefaultLayout";
 import DettaglioProdotto from "./pages/DettaglioProdotto";
+import { BudgetProvider } from "./context/BudgetContext";
 
 function App() {
   return (
-    <>
+    <BudgetProvider>
       <BrowserRouter>
         <Routes>
           <Route element={<DefaultLayout />}>
-          <Route path="/" element={<Homepage />}></Route>
-          <Route path="/chisiamo" element={<ChiSiamo />}></Route>
-          <Route index path="/prodotti" element={<Prodotti />}/>
-          <Route path="/dettaglio-prodotto/:id" element={<DettaglioProdotto />}></Route>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/chisiamo" element={<ChiSiamo />} />
+            <Route index path="/prodotti" element={<Prodotti />} />
+            <Route
+              path="/dettaglio-prodotto/:id"
+              element={<DettaglioProdotto />}
+            />
           </Route>
         </Routes>
       </BrowserRouter>
-    </>
+    </BudgetProvider>
   );
 }
 
