b1fb6b237e12452c41142158006d6d8755e42609
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-09 14:53:55 +0200

    [upd] implemented filter into budgetMode var for <= 30 value, remapped prodotti to prodottiDaMostrare, [del] React Fragment

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -12,40 +12,40 @@ const Prodotti = () => {
       setProdotti(resp.data);
     });
   }, []);
+// filtro per mostrare prodotti <= 30€
+    const prodottiDaMostrare = budgetMode
+    ? prodotti.filter((prodotto) => prodotto.price <= 30)
+    : prodotti;
 
   return (
-    <>
-      <div className="container mt-4">
-        <h1 className="mb-4">Prodotti</h1>
+    <div className="container mt-4">
+      <h1 className="mb-4">Prodotti</h1>
 
-        <div className="row">
-          {prodotti.map((prodotto) => (
-            <div className="col-md-3 mb-4" key={prodotto.id}>
-              <div className="card h-100 card shadow">
-                <img
-                  src={prodotto.image}
-                  className="card-img-top p-3"
-                  alt={prodotto.title}
-                />
-                <div className="card-body d-flex flex-column">
-                  <h5 className="card-title">{prodotto.title}</h5>
-                  <p className="card-text text-truncate">
-                    {prodotto.description}
-                  </p>
-                  <p className="fw-bold mt-auto">€ {prodotto.price}</p>
-                  <Link
-                    className="btn btn-warning fw-bold"
-                    to={`/dettaglio-prodotto/${prodotto.id}`}
-                  >
-                    Dettagli
-                  </Link>
-                </div>
+      <div className="row">
+        {prodottiDaMostrare.map((prodotto) => (
+          <div className="col-md-3 mb-4" key={prodotto.id}>
+            <div className="card h-100 shadow">
+              <img
+                src={prodotto.image}
+                className="card-img-top p-3"
+                alt={prodotto.title}
+              />
+              <div className="card-body d-flex flex-column">
+                <h5 className="card-title">{prodotto.title}</h5>
+                <p className="card-text text-truncate">{prodotto.description}</p>
+                <p className="fw-bold mt-auto">€ {prodotto.price}</p>
+                <Link
+                  className="btn btn-warning fw-bold"
+                  to={`/dettaglio-prodotto/${prodotto.id}`}
+                >
+                  Dettagli
+                </Link>
               </div>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
